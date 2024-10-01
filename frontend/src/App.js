3cf3ae657f27548db2ca3ea08c4d6c15b6e72eaf
Abrar Mohammed <hn1125@wayne.edu>
2024-10-01 00:29:42 -0400

    Updated App.js to add a logo, button, and updated text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import logo from './gg.png';  // Make sure the path to the logo is correct
 
 function App() {
-  return <div className="App">Good Gamers</div>;
+  return (
+    <div className="App">
+      <h1>Welcome to GG-Good Gamers Team!</h1>
+      <img src={logo} alt="Logo" width="200" />
+      <p>This is the ultimate platform for gamers to connect and compete.</p>
+      <button onClick={() => alert("Button clicked!")}>Click Me</button>
+    </div>
+  );
 }
 
 export default App;
