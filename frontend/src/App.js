155bde327fd0e1905ba4ef84d46fad1ff69eee3d
Mohammed Al-Musawi <hg7622@wayne.edu>
2024-10-04 05:05:16 -0400

    Initialized user login with Firebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';  // Assuming Login.js is in the 'pages' folder
-import Home from './pages/Home';    // Assuming Home.js is in the 'pages' folder
-import './App.css';                 // Global styles
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login"; // Assuming Login.js is in the 'pages' folder
+import Home from "./pages/Home"; // Assuming Home.js is in the 'pages' folder
+import "./App.css"; // Global styles
+import { AuthProvider, useAuth } from "./contexts/authContext";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {/* Route for Login Page */}
-          <Route path="/" element={<Login />} />
-          
-          {/* Route for Home Page */}
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            {/* Route for Login Page */}
+            <Route path="/" element={<Login />} />
+
+            {/* Route for Home Page */}
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
