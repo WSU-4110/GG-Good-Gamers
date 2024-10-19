4a898a03a4d675de68d4c15d73c71830497b952e
Cooper Teets <hh2078@wayne.edu>
2024-10-19 16:30:46 -0400

    I am so confused all the files are red

--- backend/api/models.py
+++ backend/api/models.py
@@ -15,39 +15,20 @@ class Project(models.Model):
 
 class User(models.Model):
     #here is where we add attributes for users
-<<<<<<< HEAD
-    userEmail = models.CharField(max_length = 64)#Primary Key
-    username = models.CharField(max_length = 30)
-    screenName = models.CharField(max_length = 20)
-    userPassword = models.CharField(max_length=25)
-    userGender = models.IntegerField() # 0 = male, 1 = female
-    userDOB = models.IntegerField()
-    postID = models.IntegerField()
-=======
     screenName = models.CharField(unique=True, max_length = 20)
     userEmail = models.EmailField(max_length = 64)
     userName = models.CharField(max_length = 30)
     userPassword = models.CharField(max_length=25)
     userGender = models.IntegerField() # 0 = male, 1 = female
     userDOB = models.DateField()
->>>>>>> 9fcb783c1fa17af71d4ec086f2dcbe63495055c8
 
 class BlacklistedUser(models.Model):
     screenName = models.CharField(max_length = 20)
-    userEmail = models.CharField(max_length = 64)#Primary Key
+    userEmail = models.CharField(max_length = 64)
 
-<<<<<<< HEAD
-class admin(models.Model):
-    userEmail = models.CharField(max_length = 64)#Primary Key
-=======
 class AdminUser(models.Model):
     userEmail = models.CharField(max_length = 64)
->>>>>>> 9fcb783c1fa17af71d4ec086f2dcbe63495055c8
     userName = models.CharField(max_length = 30)
     screenName = models.CharField(max_length = 20)
 
-class postInteraction(models.Model):
-    postID = models.IntegerField() #Primary Key
-    likeNum = models.IntegerField()
-    dislikeNum = models.IntegerField()
-    comment = models.CharField(max_length = 120)
+
