4d55a3f5e624d7a92160b549cd0aa04d52920016
Cooper Teets <hh2078@wayne.edu>
2024-10-22 16:45:31 -0400

    Saving changes before leaving for class

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 # Create your models here.
 
 class Project(models.Model):
@@ -12,39 +13,52 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+    
+class UserID(models.Model):
+    id = models.IntegerField(unique = True, default = (random.randint(0,9999999)), primary_key = True)
+    class Meta:
+        abstract = True
+        ordering = ['id']
+        db_table = "User ID Number"
 
 class User(models.Model):
     #here is where we add attributes for users
+    #rand = User.objects.make_random_password(length = 20, allowedC)
     screenName = models.CharField(unique = True, max_length = 20) 
     userEmail = models.EmailField(unique = True, max_length = 64) 
     userName = models.CharField(max_length = 30) 
     userPassword = models.CharField(max_length=25)
     userGender = models.IntegerField() # 0 = male, 1 = female
     userDOB = models.DateField()
-    userID = models.IntegerField(unique = True) #Primary Key
+    userID = models.IntegerField(unique = True, primary_key = True) #Primary Key
+    doc = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.screenName, self.userID
+    
+    
+    
 
 class BlacklistedUser(models.Model):
-    userID = models.IntegerField() #Primary Key
+    userID = models.IntegerField(unique = True) #Primary Key
 
     def __str__(self):
         return self.userID
 
 class AdminUser(models.Model):
-    userID = models.IntegerField() #Primary Key
+    userID = models.IntegerField(unique = True) #Primary Key
 
     def __str__(self):
         return self.userID
 
 class PostInteraction(models.Model):
-    userID = models.IntegerField() #Primary Key
-    postID = models.IntegerField()
+    userID = models.IntegerField(unique = True) #Primary Key
+    postID = models.IntegerField(unique = True, max_length = 64)
     created = models.DateTimeField(auto_now_add=True)
     comments = models.CharField(max_length=500, blank=True, null=True)
     likeCount = models.IntegerField() 
     dislikeCount = models.IntegerField()
+    doc = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.postID, self.likeCount, self.dislikeCount
