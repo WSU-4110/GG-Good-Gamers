8af33ace84ef943a14fd070cf3648cdadd2808b6
MOHAMED ALMASMARI <fr3002@wayne.edu>
2024-10-24 17:55:35 -0400

    create History page with two components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login"; // Assuming Login.js is in the 'pages' folder
 import Home from "./pages/Home"; // Assuming Home.js is in the 'pages' folder
+import History from "./pages/History";
 import "./App.css"; // Global styles
 import { AuthProvider, useAuth } from "./contexts/authContext";
 
@@ -16,6 +17,9 @@ function App() {
 
             {/* Route for Home Page */}
             <Route path="/home" element={<Home />} />
+
+            { /* Route for History Page*/}
+            <Route path="/history" element={<History />} />
           </Routes>
         </div>
       </Router>
